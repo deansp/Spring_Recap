496f4f2f26bf5174b8c28273654d5905f42dd0ee
deanspecht <deansp@web.de>
2024-04-10 13:45:27 +0200

    Initial commit

---
+++ src/test/java/com/example/spring_recap/SpringRecapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_recap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRecapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
